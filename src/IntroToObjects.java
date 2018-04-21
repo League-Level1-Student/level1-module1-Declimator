59aac937e4d5691efabaabf0e632e4e42d5d4165
elijahshark <league@WTS-iMac-11.attlocal.net>
2018-04-20 18:28:46 -0700

    Introduction to Objects IP

---
+++ src/IntroToObjects.java
@@ -0,0 +1,15 @@
+
+public class IntroToObjects {
+public static void main(String[] args) {
+	
+}
+}
+class Duck{
+	private String favoriteFood;
+	private int numberOfFriends;
+
+	Duck(String favoriteFood, int numberOfFriends) {
+      	this.favoriteFood = favoriteFood;
+      	this.numberOfFriends = numberOfFriends;
+}
+}
